e4415e96af32d09bf2b8f20bbef1c958519a1da5
supme <supmea@gmail.com>
2017-02-22 15:57:09 +0300

    fix example

--- example/main.go
+++ example/main.go
@@ -11,18 +11,18 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/elf/", http.StripPrefix("/elf/", http.FileServer(http.Dir("./elf/"))))
 
-	config := elFinder.Config{}
-	config["l0"] = elFinder.Volume {
+	config := goElFinder.Config{}
+	config["l0"] = goElFinder.Volume {
 		Root: "./files/1",
 		AllowDirs: []string{"/Allow"},
 		DenyDirs:  []string{"/Deny"},
 		DefaultRight: false,
 	}
-	config["l1"] = elFinder.Volume {
+	config["l1"] = goElFinder.Volume {
 		Root: "./files/2",
 		DefaultRight: true,
 	}
-	mux.Handle("/connector", elFinder.NetHttp(config))
+	mux.Handle("/connector", goElFinder.NetHttp(config))
 
 	fmt.Println("Listen on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
